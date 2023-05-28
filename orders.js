b329f560cf48f05c9da28181c17ccf7df7bc4cb8
SWATBIT <sgtech311@gmail.com>
2023-05-28 12:06:08 +0530

    day 3

---
+++ orders.js
@@ -0,0 +1,38 @@
+const Orders = [
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'LAVENDER KF102 Drone',
+        productNumber: '46385',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'Ruko F11 Pro Drone',
+        productNumber: '58964',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Drone with Camera Drone',
+        productNumber: '78541',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'GPS 4K Drone',
+        productNumber: 'Paid',
+        paymentStatus: 'Due',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'DJI Air 25',
+        productNumber: '84715',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    }
+]
